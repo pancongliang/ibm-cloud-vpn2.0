2c38d1f308925e918304d0cde8fafe81b819352e
pancongliang <75927592+pancongliang@users.noreply.github.com>
2024-07-31 16:15:07 +0800

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo -n Password:
-read -s password
-source <(grep = vpn.ini)
-printf "\n$method $host $user $log\n"
-
-sudo podman run --rm -it --name VPNcontainer --privileged --network host ibm-cloud-vpn2.0 --method $method --host $host --user $user --passwd $password --loglevel $log 
-
+    source <(grep = vpn.ini)
+    printf "\n$method $host $user $log\n"
+    
+    sudo podman run -d --name VPNcontainer --privileged ibm-cloud-vpn2.0 --method $method --host $host --user $user --passwd $PASSWD --loglevel $log
